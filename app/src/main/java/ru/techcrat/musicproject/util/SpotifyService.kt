bf87900eaeab3aadfe0f8a9e95bda8a23a13fc49
Artem <asemakin403@gmail.com>
2021-02-15 17:09:23 +0300

    service

--- app/src/main/java/ru/techcrat/musicproject/util/SpotifyService.kt
+++ app/src/main/java/ru/techcrat/musicproject/util/SpotifyService.kt
@@ -1,14 +1,12 @@
-package ru.techcrat.musicproject.model
-
 import android.content.Context
-import android.util.Log
 import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
 
 object SpotifyService {
+
+    private const val REDIRECT_URI = "musicproject://callback"
     private const val CLIENT_ID = "fbaa28eb256b47ada72a8bc2a16f0fb5"
-    private const val REDIRECT_URI = "ru.techcrat.musicproject://callback"
 
     private var spotifyAppRemote: SpotifyAppRemote? = null
     private var connectionParams: ConnectionParams = ConnectionParams.Builder(CLIENT_ID)
@@ -28,12 +26,37 @@ object SpotifyService {
             }
 
             override fun onFailure(throwable: Throwable) {
-                Log.e("SpotifyService", throwable.message, throwable)
                 handler(false)
             }
         }
         SpotifyAppRemote.connect(context, connectionParams, connectionListener)
-
-
     }
+    fun play(uri: String) {
+        spotifyAppRemote?.playerApi?.play(uri)
+    }
+
+    fun resume() {
+        spotifyAppRemote?.playerApi?.resume()
+    }
+
+    fun pause() {
+        spotifyAppRemote?.playerApi?.pause()
+    }
+
+    fun playingState(handler: (PlayingState) -> Unit) {
+        spotifyAppRemote?.playerApi?.playerState?.setResultCallback { result ->
+            if (result.track.uri == null) {
+                handler(PlayingState.STOPPED)
+            } else if (result.isPaused) {
+                handler(PlayingState.PAUSED)
+            } else {
+                handler(PlayingState.PLAYING)
+            }
+        }
+    }
+}
+
+
+enum class PlayingState {
+    PAUSED, PLAYING, STOPPED
 }
